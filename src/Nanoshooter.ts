ff43204032a6898912ab63dc95fdb1b178da668c
Chase Moskal <chasemoskal@gmail.com>
2016-05-22 23:52:22 -0700

    Render a demo scene.

--- src/Nanoshooter.ts
+++ src/Nanoshooter.ts
@@ -7,11 +7,6 @@
  * @todo: Everything.
  */
 export default class Nanoshooter {
-    canvas: HTMLCanvasElement;
-    engine: BABYLON.Engine;
-
-    /** Game initialization duration, in milliseconds. */
-    startupDuration: number;
 
     /**
      * Initialize an instance of the Nanoshooter game.
@@ -21,10 +16,37 @@ export default class Nanoshooter {
     constructor(hostElement: HTMLElement) {
         const startTime = (+new Date);
 
-        this.canvas = document.createElement("canvas");
-        this.engine = new BABYLON.Engine(this.canvas, true);
+        // Establish the canvas, insert it into the host element.
+        const canvas = document.createElement("canvas");
+        hostElement.appendChild(canvas);
 
-        this.startupDuration = (+new Date) - startTime;
+        // Initialize the Babylon engine.
+        const engine = new BABYLON.Engine(canvas, true);
+
+        // Create a demo scene.
+        const scene = (() => {
+            const scene = new BABYLON.Scene(engine);
+            scene.clearColor = new BABYLON.Color3(0, 0.1, 0);
+            const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+            camera.setTarget(BABYLON.Vector3.Zero());
+            camera.attachControl(canvas, false);
+            const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+            light.intensity = .5;
+            const sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+            sphere.position.y = 1;
+            const ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+            return scene;
+        })();
+
+        // Initiate render loop.
+        engine.runRenderLoop(() => scene.render());
+
+        // Handle browser resize.
+        window.addEventListener("resize", () => engine.resize());
+
+        // Log game initialization duration.
+        const startupDuration = (+new Date) - startTime;
+        console.debug(`Game took ${startupDuration.toFixed(0)}ms to initialize.`);
     }
 
     /**
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,3 @@ const nanoshooter = new Nanoshooter(document.body);
 
 // Attach nanoshooter to the global window object for easy debugging.
 (<any>window).nanoshooter = nanoshooter;
-
-// Log game startup time.
-console.debug(`Game took ${nanoshooter.startupDuration.toFixed(0)}ms to initialize.`);

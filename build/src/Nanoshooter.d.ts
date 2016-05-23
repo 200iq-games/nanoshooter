7843ece75818688a3dac928af71dece27c87a0a3
Chase Moskal <chasemoskal@gmail.com>
2016-05-22 22:57:42 -0700

    Fix typings and add tsconfig ignores.

--- build/src/Nanoshooter.d.ts
+++ build/src/Nanoshooter.d.ts
@@ -1,5 +1,5 @@
-/// <reference path="../bower_components/babylonjs/dist/babylon.2.3.d.ts" />
-/// <reference path="../typings/browser.d.ts" />
+/// <reference path="../../bower_components/babylonjs/dist/babylon.2.3.d.ts" />
+/// <reference path="../../typings/browser.d.ts" />
 /**
  * Open source web game.
  * @todo: Everything.

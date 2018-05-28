8e17dbebd1cea2f7f6151141d9fa793b3003c8c4
Chase Moskal <chasemoskal@gmail.com>
2018-05-27 23:04:15 -0700

    update engine

--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,7 @@
 
-import {makeGame} from "monarch-engine/dist/common/game"
+import {Game} from "monarch-engine/dist/game"
+import {Entity} from "monarch-engine/dist/entity"
+import {ModeOfConduct} from "monarch-engine/dist/interfaces"
 import {Cube, CubeEntry} from "monarch-engine/dist/common/entities/cube"
 import {Agent, AgentEntry} from "monarch-engine/dist/common/entities/agent"
 import {Editor, EditorEntry} from "monarch-engine/dist/common/entities/editor"
@@ -8,9 +10,8 @@ import {Director, DirectorEntry} from "monarch-engine/dist/common/entities/direc
 import {Spectator, SpectatorEntry} from "monarch-engine/dist/common/entities/spectator"
 import {Environment, EnvironmentEntry} from "monarch-engine/dist/common/entities/environment"
 
-import {Entity} from "monarch-engine/dist/entity"
-
-const {monarch} = makeGame({
+const game = new Game({
+	mode: ModeOfConduct.Alone,
 	canvas: document.querySelector("canvas"),
 	overlay: document.querySelector(".overlay"),
 	entityClasses: {
@@ -24,16 +25,16 @@ const {monarch} = makeGame({
 	}
 })
 
-monarch.manager.addEntry<TerrainEntry>({
+game.manager.addEntry<TerrainEntry>({
 	type: "Terrain",
 	worldmongerPath: "assets/worldmonger"
 })
 
-monarch.manager.addEntry<DirectorEntry>({
+game.manager.addEntry<DirectorEntry>({
 	type: "Director"
 })
 
-monarch.manager.addEntry<EditorEntry>({
+game.manager.addEntry<EditorEntry>({
 	type: "Editor",
 	bearings: {
 		position: [0, 25, -5],
